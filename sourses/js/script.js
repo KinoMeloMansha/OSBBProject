c5410ec24ac9c9fcb444994d7e1d032c91d94e22
Верстюк Надежда <nadia.ver@i.ua>
2018-01-19 17:38:24 +0200

    archive-carousel-first-edition

--- sourses/js/script.js
+++ sourses/js/script.js
@@ -11,11 +11,41 @@ function getTopDate() {
     
     var topDate = new Date();
     
-    $('#nav-date').append(
+    $("#nav-date").append(
         '<span class="date-day">' + topDate.getDate() + '</span>' + '<span class="date-meta">' +
             '<span class="date-month">' + topDate.getMonthName() + '</span>' + '<br>' +
             '<span class="date-year">' + topDate.getFullYear() + '</span>' + '</span>'
     );
+}
+
+function carouselStart() {
+    "use strict";
+    
+    function prevCarousel(carousel, itemWidth, speed) {
+        $(carousel).find(".carousel-items .carousel-item").eq(-1).clone().prependTo($(carousel).find(".carousel-items"));
+        $(carousel).find(".carousel-items").css({"left": "-" + itemWidth + "px"});
+        $(carousel).find(".carousel-items .carousel-item").eq(-1).remove();
+        $(carousel).find(".carousel-items").animate({left: "0px"}, speed);
+    }
+    
+    function nextCarousel(carousel, itemWidth, speed) {
+        $(carousel).find(".carousel-items .carousel-item").eq(0).clone().appendTo($(carousel).find(".carousel-items"));
+        $(carousel).find(".carousel-items").css({"left": "0px"});
+        $(carousel).find(".carousel-items .carousel-item").eq(0).remove();
+        $(carousel).find(".carousel-items").animate({left: "-" + itemWidth + "px"}, speed);
+    }
+    
+    $(".carousel-button").on("click", function () {
+        var id = $(this).attr("id"), speed = 800, carousel = $(this).parents(".carousel"), itemWidth = $(carousel).find(".carousel-item").outerWidth();
+        
+        if (id === "carousel-button-prev") {
+            prevCarousel(carousel, itemWidth, speed);
+            return false;
+            
+        } else if (id === "carousel-button-next") {
+            nextCarousel(carousel, itemWidth, speed);
+        }
+    });
 }
 
 // Document ready function
@@ -23,8 +53,5 @@ $(document).ready(function () {
     "use strict";
     
     getTopDate();
-    $('.carousel').carousel({
-    interval: 3000,
-    pause: "true"
-    });
+    carouselStart();
 });
